e69e4f70690bc45335f5bd926f198b6759039843
Teodor Avramov <64785271+DumanBG@users.noreply.github.com>
2021-03-02 11:28:56 +0200

    Add files via upload

---
+++ Inheritance/Farm/Cat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+{
+    public class Cat
+        :Animal
+    {
+
+
+        public Cat()
+        {
+
+        }
+
+
+
+
+        public void Meow()
+        {
+            Console.WriteLine("mewing...");
+
+
+        }
+    }
+
+}
---
+++ Inheritance/Farm/Puppy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farm
+{
+    public class Puppy
+        :Dog
+    {
+
+
+
+
+        public Puppy()
+        {
+
+        }
+
+
+
+
+
+
+        public void Weep()
+        {
+            Console.WriteLine("weeping...");
+        }
+    }
+}
