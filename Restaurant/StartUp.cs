5eca71e4c79a24637e25d2c931eae31d12f689a3
Teodor Avramov <64785271+DumanBG@users.noreply.github.com>
2021-03-10 15:27:41 +0200

    Add files via upload

---
+++ Restaurant/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Restaurant
+{
+  public  class StartUp
+    {
+       public static void Main(string[] args)
+        {
+
+            var coffee = new Coffee("Costa", 2.1);
+            Console.WriteLine($" Coffe price: {coffee.Price :F2}");
+
+        }
+    }
+}
