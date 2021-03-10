673e62cec6b361820bd047efbff8e40e2515dec7
Teodor Avramov <64785271+DumanBG@users.noreply.github.com>
2021-03-10 16:40:15 +0200

    Add files via upload

---
+++ Zoo/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Zoo
+{
+   public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+
+            Animal bear = new Bear("Bear");
+            Reptile snake = new Snake("Snake");
+
+
+        }
+    }
+}
