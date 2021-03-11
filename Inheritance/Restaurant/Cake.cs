244aca2743b475c852e987f3274b07b3c1d3c08c
Teodor Avramov <64785271+DumanBG@users.noreply.github.com>
2021-03-11 16:12:20 +0200

    Add files via upload

---
+++ Inheritance/Restaurant/Cake.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        private const decimal DeffaultPrice = 5M;
+        private const double DeffaultGrams = 250;
+        private const double DeffaultCaloriers = 1000;
+        public Cake(string name) 
+            : base(name, DeffaultPrice, DeffaultGrams, DeffaultCaloriers)
+        {
+        }
+    }
+}
---
+++ Inheritance/Restaurant/Coffee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const double DefaultMililiters = 50.0;
+        private const decimal DefaultPrice = 3.50M;
+
+
+        public Coffee(string name, double caffeine) 
+            : base(name, DefaultPrice, DefaultMililiters)  // 3.50M . 50
+        {
+            this.Caffeine = caffeine;
+        }
+
+        public double Caffeine { get; set; }
+
+
+
+    }
+}
