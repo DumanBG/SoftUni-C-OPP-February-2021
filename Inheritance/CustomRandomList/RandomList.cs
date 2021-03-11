860d484527319b468d2eb4070fdc4a4586cad4e9
Teodor Avramov <64785271+DumanBG@users.noreply.github.com>
2021-03-11 16:09:57 +0200

    Add files via upload

---
+++ Inheritance/CustomRandomList/RandomList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList<T> : List<T>
+    {
+        private Random rnd;
+
+        public RandomList()
+        {
+            rnd = new Random();
+
+        }
+
+
+
+        public void Add(T el)
+        {
+            base.Add(el);
+            Console.WriteLine($"Added the string {el} and we have custom functionalities");
+        }
+
+        //public string RemoveRandomElement()
+
+        //{
+        //    int index = rnd.Next(0, this.Count);
+        //    string str = this[index];
+        //   //  this e стринг RemoveRandomElement
+        //    this.RemoveAt(index);
+        //    return str;
+        //}
+
+        public T RandomString()
+        {
+            return this[rnd.Next(0, this.Count)];
+        }
+    }
+}
---
+++ Inheritance/CustomRandomList/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+       public static void Main(string[] args)
+        {
+
+
+
+            RandomList<string> list = new RandomList<string>();
+
+            list.Add("Duman");
+            list.Add("Teodor");
+            list.Add("Pesho");
+            list.Add("Azar");
+            list.Add("Mudan");
+
+
+            for (int i = 0; i < 1; i++)
+            {
+                Console.WriteLine(list.RandomString());
+            }
+            foreach (var item in list)
+            {
+                Console.WriteLine(list.RandomString());
+            }
+        }
+    }
+}
