c2d39625970c7fa006bd73ad408efef6a763a6c9
Teodor Avramov <64785271+DumanBG@users.noreply.github.com>
2021-03-18 19:28:08 +0200

    Add files via upload

---
+++ ENCAPSULATION/ShoppingSpree/Person.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace ShoppingSpree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> products;
+
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+            this.products = new List<Product>();
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                this.name = value;
+            }
+        }
+
+        public decimal Money
+        {
+            get { return this.money; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                this.money = value;
+            }
+        }
+        public IList<Product> GetProducts()
+        {
+            return this.products.AsReadOnly();
+        }
+
+        public void BuyProduct(Product product)
+        {
+            if (this.Money < product.Price)
+            {
+                throw new InvalidOperationException($"{this.Name} can't afford {product.Name}");
+            }
+
+            this.products.Add(product);
+            this.money -= product.Price;
+        }
+
+        public override string ToString()
+        {
+            if(this.products.Count ==0)
+            {
+                return $"{this.Name} - Nothing bought";
+            }
+            return $"{this.Name} - {string.Join(", ",this.products.Select(p=>p.Name))}".TrimEnd();
+        }
+
+    }
+}
---
+++ ENCAPSULATION/ShoppingSpree/StartUp.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShoppingSpree
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Person> people = new Dictionary<string, Person>();
+            Dictionary<string, Product> products = new Dictionary<string, Product>();
+            try
+            {
+                people = ReadPeople();
+                products = ReadProducts();
+            }
+            catch (ArgumentException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            while(true)
+            {
+                string line = Console.ReadLine();
+                if(line == "END")
+                {
+                    break;
+                }
+                string[] parts = line.Split();
+               
+                string personName = parts[0];
+                string productName = parts[1];
+
+                Person person = people[personName];
+                Product product = products[productName];
+                try
+                {
+                person.BuyProduct(product);
+
+                Console.WriteLine($"{personName} bought {productName}");
+
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            foreach (var person in people.Values)
+            {
+                Console.WriteLine(person);
+            }
+
+        }
+
+        private static Dictionary<string, Product> ReadProducts()
+        {
+            Dictionary<string, Product> result = new Dictionary<string, Product>();
+            string[] parts = Console.ReadLine().Split(";", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string part in parts)
+            {
+                string[] productData = part.Split("=");
+                string productName = productData[0];
+                decimal productCost = decimal.Parse(productData[1]);
+
+                result[productName] = new Product(productName, productCost);
+            }
+            return result;
+        }
+
+        private static Dictionary<string, Person> ReadPeople()
+        {
+            Dictionary<string,Person> result = new Dictionary<string, Person>();
+
+            string[] parts = Console.ReadLine().Split(";");
+
+            foreach (string part in parts)
+            {
+                string[] personData = part.Split("=", StringSplitOptions.RemoveEmptyEntries);
+                string name = personData[0];
+                decimal money = decimal.Parse(personData[1]);
+
+                result[name] = new Person(name, money);
+            }
+
+
+                return result;
+        }
+    }
+}
