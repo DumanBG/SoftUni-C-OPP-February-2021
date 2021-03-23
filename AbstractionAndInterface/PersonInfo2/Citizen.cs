25a80b8f57e92eb49760cb7b27d88a69abca31e8
Teodor Avramov <64785271+DumanBG@users.noreply.github.com>
2021-03-23 19:22:56 +0200

    Add files via upload

---
+++ AbstractionAndInterface/PersonInfo2/Citizen.cs
@@ -0,0 +1,31 @@
+﻿using PersonInfo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson, IBirthable, IIdentifiable
+    {
+
+        string name;
+        int age;
+        string birthdate;
+        string id;
+        // IIdentifiable identifiable = new Citizen(name, age, id, birthdate);
+        //IBirthable birthable = new Citizen(name, age, id, birthdate);
+        public Citizen(string name, int age, string id, string birthdate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Birthdate = birthdate;
+            this.Id = id;
+        }
+        public string Name { get => this.name; private set => this.name = value; }
+        public int Age { get => this.age; private set => this.age = value; }
+
+        public string Birthdate { get => this.birthdate; private set => this.birthdate = value; }
+
+        public string Id { get => this.id; private set => this.id = value; }
+    }
+}
---
+++ AbstractionAndInterface/PersonInfo2/IPerson.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+   public interface IPerson 
+    {
+
+        string Name { get; } // Само през Ctor-a да се имплементира
+        int Age { get; }
+    }
+}
