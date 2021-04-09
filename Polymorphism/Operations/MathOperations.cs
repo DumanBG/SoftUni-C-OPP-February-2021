95bfc54c8eb39de79d5aad05814a3cb74035a2ad
Teodor Avramov <64785271+DumanBG@users.noreply.github.com>
2021-04-09 17:30:51 +0300

    Add files via upload

---
+++ Polymorphism/Operations/MathOperations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operations
+{
+    public class MathOperations
+    {
+
+        public MathOperations()
+        {
+
+        }
+
+        public int A { get; set; }
+        public int B { get; set; }
+
+        public  int  Add(int a, int b)
+        {
+            return a + b;
+        }
+        public  double Add(double a, double b, double c)
+        {
+            return a + b;
+        }
+        public decimal  Add(decimal a, decimal b, decimal c)
+        {
+            return a + b+c;
+        }
+    }
+}
